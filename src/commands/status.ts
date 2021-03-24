1177281cb06a3eec08b4dd6a535b96ecde6de7d0
Sören <hello@soerenschwert.de>
2021-03-24 11:39:05 +0100

    Update status to include more information

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,14 +1,24 @@
 import {RpcCommand} from '../lib/rpc-command'
+import chalk from 'chalk'
 
 export default class Status extends RpcCommand {
   static description = 'Show the current status of the node'
 
   async run() {
-    const blockNumber = await this.$rpc.call('blockNumber')
-    // TODO: Get connected peers
-    // TODO: Get consensus status
-    // TODO: Get validator status
+    const [
+      blockNumber,
+      epochNumber,
+      established,
+    ] = await Promise.all([
+      this.$rpc.call('getBlockNumber'),
+      this.$rpc.call('getEpochNumber'),
+      this.$rpc.call('isEstablished'),
+      // TODO: Get connected peers
+      // TODO: Get validator status
+    ])
 
     this.log('Height:', blockNumber)
+    this.log('Epoch:', epochNumber)
+    this.log('Consensus:', established ? chalk.green('established') : chalk.yellow('syncing'))
   }
 }
